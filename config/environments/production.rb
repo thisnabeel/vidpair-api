87e4913153f87258aa9eacaa510c2721e77ae2e6
thisnabeel <nabeel.khan.swe@gmail.com>
2026-01-28 01:06:56 -0800

    fix prod connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   config.hosts = [
     "vidpair-api-production.up.railway.app"
   ]
+  
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
